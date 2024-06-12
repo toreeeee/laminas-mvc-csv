28af6b2575c7cd90cd0217d9f354ceae3698a1d7
edgar.nentzl <edgar.nentzl@check24.de>
2024-06-12 07:46:21 +0200

    feat: add more tests for `TableFile`

--- module/Person/test/Service/TableFileTest.php
+++ module/Person/test/Service/TableFileTest.php
@@ -5,12 +5,26 @@ namespace PersonTest\Service;
 use Person\Service\CSVFile\CSVRow;
 use Person\Service\TableFile;
 use PHPUnit\Framework\TestCase;
+use TableRow;
 
 class TableFileTest extends TestCase
 {
+    public function testEmpty()
+    {
+        $file = new TableFile([], []);
+
+        $this->assertSame([], $file->getRows());
+        $this->assertSame([], $file->getHeadings());
+        $this->assertSame([], $file->getInvalidRows());
+        $this->assertSame([], $file->getValidRows());
+    }
+
     public function testGetRows()
     {
-        $rows = [new CSVRow(["first", "second", "third"]), new CSVRow(["4", "5", "6"])];
+        $rows = [
+            new CSVRow(["first", "second", "third"]),
+            new CSVRow(["4", "5", "6"]),
+        ];
 
         $file = new TableFile([], $rows);
 
@@ -26,5 +40,53 @@ class TableFileTest extends TestCase
         $this->assertSame($headings, $file->getHeadings());
     }
 
-    // TODO: test the rest of TableFile
+    public function testReturnsSameAmountAsInput()
+    {
+        $inputRows = [
+            new CSVRow(["test column"], 2),
+            new CSVRow(["test column"], 1),
+            new CSVRow(["test column", "test", "value"], 3),
+            new CSVRow(["test column", "v"], 1),
+        ];
+
+        $table = new TableFile(
+            [],
+            $inputRows
+        );
+
+        $this->assertSame($inputRows, $table->getRows());
+        $this->assertSame(count($inputRows), count($table->getInvalidRows()) + count($table->getValidRows()));
+    }
+
+    public function testGetValidRows()
+    {
+        $row = new CSVRow(["test", "column"], 2);
+
+        $table = new TableFile(
+            [],
+            [
+                new CSVRow(["test column"], 2),
+                $row,
+            ]
+        );
+
+        $this->assertContains($row, $table->getValidRows());
+        $this->assertSame(1, count($table->getValidRows()));
+    }
+
+    public function testGetInvalidRows()
+    {
+        $row = new CSVRow(["test", "column"], 3);
+
+        $table = new TableFile(
+            [],
+            [
+                new CSVRow(["test column"], 1),
+                $row,
+            ]
+        );
+
+        $this->assertContains($row, $table->getInvalidRows());
+        $this->assertSame(1, count($table->getInvalidRows()));
+    }
 }
